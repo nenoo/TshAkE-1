9160dc132b47b706b1b4a2e4665e7cd658c29b85
TEAM TSHAKE <nadoo.ali.15.4@gmail.com>
2017-04-21 05:57:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,4 +50,6 @@ wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
  wget "https://valtman.name/files/telegram-cli-1222"
  mv telegram-cli-1222 tg
  chmod +x tg
+ chmod +x TSHAKE.sh
+ chmod +x TSHAKE-Auto.sh
  ./TSHAKE.sh
