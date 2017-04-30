03cda1658f4696a64a58dbca4fd1474750015258
TEAM TSHAKE <nadoo.ali.15.4@gmail.com>
2017-04-30 21:41:05 +0300

    Update TSHAKE-Auto.sh

--- TSHAKE-Auto.sh
+++ TSHAKE-Auto.sh
@@ -1,3 +1,7 @@
-while true; do
+#!/bin/bash
+COUNTER=1
+while(true) do
 ./TSHAKE.sh
+curl "https://api.telegram.org/bot[362136053:AAGkJVHSOaAHFADlrQmKDn28AGLDNIbEA5c]/sendmessage" -F "chat_id=67369633" -F "text=#NEWCRASH-#TeleBeyond-Reloaded-${COUNTER}-times"
+let COUNTER=COUNTER+1 
 done
